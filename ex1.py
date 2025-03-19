3f9fb4e94f2e365a25e164115bfb848511fa4c70
boopitis <stephenravelo31@gmail.com>
2025-03-19 12:31:46 -0600

    Complete ex1

---
+++ ex1.py
@@ -0,0 +1,92 @@
+import timeit
+import random
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import sys
+sys.setrecursionlimit(11000)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def balance(self, node):
+        if not node:
+            return 0
+        return self.height(node.left) - self.height(node.right)
+
+    def insert(self, root, value):
+        if not root:
+            return Node(value)
+        elif value < root.value:
+            root.left = self.insert(root.left, value)
+        else:
+            root.right = self.insert(root.right, value)
+
+        root.height = 1 + max(self.height(root.left), self.height(root.right))
+        balance = self.balance(root)
+        
+        return root
+    
+    def search(self, root, value):
+        if not root or root.value == value:
+            return root
+        if root.value < value:
+            return self.search(root.right, value)
+        return self.search(root.left, value)
+    
+    def insert_value(self, value):
+        self.root = self.insert(self.root, value)
+        
+    def search_value(self, value):
+        return self.search(self.root, value)
+
+def doTasks(tree, tasks):
+    for task in tasks:
+        tree.search_value(task)
+
+if __name__ == "__main__":
+    largest_absolute_balances = []
+    
+    tasks = list(range(1000))
+    
+    avg_times = []
+    
+    for i in range(1000):
+        tree = AVLTree()
+        random.shuffle(tasks)
+        for task in tasks:
+            tree.insert_value(task)
+        
+        largest_absolute_balance = abs(tree.balance(tree.root))
+        largest_absolute_balances.append(largest_absolute_balance)
+        time = timeit.timeit(lambda: doTasks(tree, tasks), number=1)
+        avg = time
+        avg_times.append(avg)
+        
+        df_data = np.array([[avg, largest_absolute_balance]])
+        df = pd.DataFrame(df_data, columns=['avg time (ms)', 'largest absolute balance'])
+        print(df.to_string(index=False))
+
+    ax = plt.figure().add_subplot()
+    ax.scatter(largest_absolute_balances, avg_times,  c="red")
+    ax.set_title("largest absolute balance vs. average time")
+    ax.set_xlabel("largest absolute balance")
+    ax.set_ylabel("average time (ms)")
+    ax.set_xlim(0)
+    ax.set_ylim(0)
+    ax.legend()
+    plt.show()
